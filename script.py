61123b38829d4dd48c4d41cd984eff8b8ff9f64b
Electrochock1974 <114209777+Electrochock1974@users.noreply.github.com>
2023-04-03 11:44:29 +0200

    Update script.py

--- script.py
+++ script.py
@@ -6,17 +6,19 @@
 #python script.py
 #/!\ il est important que le fichier script.py sois dans un dossier pour
 #plus de simplicité.
+#/!\ tout les fichier sont supprimé si vous relancer le script.
 import requests
 import os
 import json
+import glob
 
 # Supprimer le fichier data.json s'il existe
 if os.path.exists('data.json'):
     os.remove('data.json')
 
-# Supprimer le fichier top_5_crypto.txt s'il existe
-if os.path.exists('top_5_crypto.txt'):
-    os.remove('top_5_crypto.txt')
+# Supprimer tous les fichiers qui commencent par "top_5_crypto_" et se terminent par ".txt"
+for file in glob.glob("top_5_crypto_*.txt"):
+    os.remove(file)
 
 # Supprimer les dossiers top_gainers et top_losers s'ils existent
 if os.path.exists("top_gainers"):
@@ -24,13 +26,26 @@ if os.path.exists("top_gainers"):
 if os.path.exists("top_losers"):
     os.system("rmdir /s /q top_losers")
 
+# Durées disponibles
+durees_disponibles = {'1h': 'priceChange1h',
+                      '24h': 'priceChange24h',
+                      '7d': 'priceChange7d',
+                      '30d': 'priceChange30d'}
 
+# Durée par défaut
+duree_par_defaut = '7d'
+
+# Demander la durée à l'utilisateur
+print(f"Veuillez choisir la durée pour le classement (parmi {', '.join(durees_disponibles.keys())}, par défaut {duree_par_defaut}): ")
+duree = input().strip()
+
+# Vérifier si la durée est valide, sinon utiliser la durée par défaut
+if duree not in durees_disponibles:
+    print(f"Pas de durée valide selectionné. La durée de {duree_par_defaut} à été utilisé.")
+    duree = duree_par_defaut
 
 # Obtenir les données JSON de l'API CoinMarketCap
-# A modifier : changer l'option de durée (1h, 24h, 7d, 30d) selon vos besoins
-timeframe = '7d'
-
-url = f'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/spotlight?dataType=2&limit=30&rankRange=100&timeframe={timeframe}'
+url = f'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/spotlight?dataType=2&limit=30&rankRange=100&timeframe={duree}'
 response = requests.get(url)
 data = response.json()
 
@@ -42,25 +57,23 @@ with open('data.json', 'w') as f:
 with open('data.json', 'r') as f:
     data = json.load(f)
 
-# Récupérer les 5 meilleures cryptos pour la durée de 7 jours
-gainers = sorted([item for item in data['data']['gainerList'] if item.get('priceChange', {}).get('priceChange7d') is not None], key=lambda x: x['priceChange']['priceChange7d'], reverse=True)[:5]
+# Récupérer les 5 meilleures cryptos pour la durée demandée
+gainers = sorted([item for item in data['data']['gainerList'] if item.get('priceChange', {}).get(durees_disponibles[duree]) is not None], key=lambda x: x['priceChange'][durees_disponibles[duree]], reverse=True)[:5]
 
-# Récupérer les 5 pires cryptos pour la durée de 7 jours
-losers = sorted([item for item in data['data']['loserList'] if item.get('priceChange', {}).get('priceChange7d') is not None], key=lambda x: x['priceChange']['priceChange7d'])[:5]
+# Récupérer les 5 pires cryptos pour la durée demandée
+losers = sorted([item for item in data['data']['loserList'] if item.get('priceChange', {}).get(durees_disponibles[duree]) is not None], key=lambda x: x['priceChange'][durees_disponibles[duree]])[:5]
 
 # Créer les dossiers d'images s'ils n'existent pas
 if not os.path.exists("top_gainers"):
     os.makedirs("top_gainers")
 if not os.path.exists("top_losers"):
     os.makedirs("top_losers")
-
 # Télécharger les images pour les 5 meilleures cryptos
 for crypto in gainers:
     img_url = f"https://s2.coinmarketcap.com/static/img/coins/128x128/{crypto['id']}.png"
     img_data = requests.get(img_url).content
     with open(f"top_gainers/{crypto['id']}.png", 'wb') as f:
         f.write(img_data)
-
 # Télécharger les images pour les 5 pires cryptos
 for crypto in losers:
     img_url = f"https://s2.coinmarketcap.com/static/img/coins/128x128/{crypto['id']}.png"
@@ -68,14 +81,28 @@ for crypto in losers:
     with open(f"top_losers/{crypto['id']}.png", 'wb') as f:
         f.write(img_data)
 
-# Créer le fichier texte avec le classement pour les 7 jours
-with open('top_5_crypto.txt', 'w', encoding='utf-8') as f:
-    f.write("Les 5 meilleures cryptos pour la durée de 7 jours :\n")
+
+# Créer le fichier texte avec le classement pour la durée sélectionnée
+if duree == '1h':
+    timeframe_text = '1 heure'
+    time_key = 'priceChange1h'
+elif duree == '24h':
+    timeframe_text = '24 heures'
+    time_key = 'priceChange24h'
+elif duree == '7d':
+    timeframe_text = '7 jours'
+    time_key = 'priceChange7d'
+else:
+    timeframe_text = '30 jours'
+    time_key = 'priceChange30d'
+
+with open(f'top_5_crypto_{duree}.txt', 'w', encoding='utf-8') as f:
+    f.write(f"Les 5 meilleures cryptos pour la durée de {timeframe_text} :\n")
     for i, crypto in enumerate(gainers):
         rank = i + 1
-        f.write(f"{rank}. [{crypto['id']}] {crypto['name']} ({crypto['symbol']}): {crypto['priceChange']['priceChange7d']}% de gain\n")
+        f.write(f"{rank}. [{crypto['id']}.png] {crypto['name']} ({crypto['symbol']}): {crypto['priceChange'][time_key]}% de gain\n")
 
-    f.write("\nLes 5 pires cryptos pour la durée de 7 jours :\n")
+    f.write(f"\nLes 5 pires cryptos pour la durée de {timeframe_text} :\n")
     for i, crypto in enumerate(losers):
         rank = i + 1
-        f.write(f"{rank}. [{crypto['id']}] {crypto['name']} ({crypto['symbol']}): {crypto['priceChange']['priceChange7d']}% de perte\n")
+        f.write(f"{rank}. [{crypto['id']}.png] {crypto['name']} ({crypto['symbol']}): {crypto['priceChange'][time_key]}% de perte\n")
